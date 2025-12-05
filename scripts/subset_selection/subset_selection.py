b4568b498cedd7795c93f23fc90909d195d3cc3e
Shruthi Sankepelly <shruthi.s4@gmail.com>
2025-12-05 18:17:54 -0500

    move the setup of spwan post torch import

--- scripts/subset_selection/subset_selection.py
+++ scripts/subset_selection/subset_selection.py
@@ -3,9 +3,7 @@ from dataclasses import dataclass, field
 from multiprocessing import Pool
 import multiprocessing as mp
 
-# Only use spawn method if CUDA is available, otherwise use default method
-if torch.cuda.is_available() and mp.get_start_method(allow_none=True) != 'spawn':
-    mp.set_start_method('spawn', force=True)
+
     
 from typing import Any, Dict, List, TypedDict, TypeVar, Union
 import gc
@@ -23,6 +21,10 @@ import h5py
 import numpy as np
 import torch
 
+# Only use spawn method if CUDA is available, otherwise use default method
+if torch.cuda.is_available() and mp.get_start_method(allow_none=True) != 'spawn':
+    mp.set_start_method('spawn', force=True)
+    
 # Local
 from .encoders import get_encoder_class
 from .utils.subset_selection_utils import (
