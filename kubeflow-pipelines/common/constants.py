db32b00dbffc607fdb71312163b9fb11f146dd69
Shruthi Sankepelly <shruthi.s4@gmail.com>
2025-12-22 14:45:46 -0500

    Add support for customizable base images in local runner workflow
    Enhanced the execute-kfp-localrunners workflow to allow overriding of PYTHON_BASE_IMAGE and DOCLING_BASE_IMAGE through input parameters. Updated constants to use environment variables for base images.

--- kubeflow-pipelines/common/constants.py
+++ kubeflow-pipelines/common/constants.py
@@ -1,3 +1,5 @@
+import os
+
 # Base container images used across all Docling Kubeflow Pipelines
-PYTHON_BASE_IMAGE = "quay.io/amaredia/aipcc-docling-image" #"quay.io/aipcc/docling/cuda-ubi9"
-DOCLING_BASE_IMAGE = "quay.io/amaredia/aipcc-docling-image"
+PYTHON_BASE_IMAGE = os.getenv("PYTHON_BASE_IMAGE", "quay.io/amaredia/aipcc-docling-image")
+DOCLING_BASE_IMAGE = os.getenv("DOCLING_BASE_IMAGE", "quay.io/amaredia/aipcc-docling-image")
