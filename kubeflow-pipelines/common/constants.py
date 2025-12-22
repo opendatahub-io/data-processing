05f549550a8f6d6f8e0fd02cec08bea417bef270
Shruthi Sankepelly <shruthi.s4@gmail.com>
2025-12-22 14:55:20 -0500

    Run  ruff-format on constants.py

--- kubeflow-pipelines/common/constants.py
+++ kubeflow-pipelines/common/constants.py
@@ -1,5 +1,9 @@
 import os
 
 # Base container images used across all Docling Kubeflow Pipelines
-PYTHON_BASE_IMAGE = os.getenv("PYTHON_BASE_IMAGE", "quay.io/amaredia/aipcc-docling-image")
-DOCLING_BASE_IMAGE = os.getenv("DOCLING_BASE_IMAGE", "quay.io/amaredia/aipcc-docling-image")
+PYTHON_BASE_IMAGE = os.getenv(
+    "PYTHON_BASE_IMAGE", "quay.io/amaredia/aipcc-docling-image"
+)
+DOCLING_BASE_IMAGE = os.getenv(
+    "DOCLING_BASE_IMAGE", "quay.io/amaredia/aipcc-docling-image"
+)
